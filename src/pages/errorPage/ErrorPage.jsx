7395c1c65c017f0225a967e1e0f3c8808c138af2
DeeBest <simphiwedladla8@gmail.com>
2025-02-04 13:50:51 +0200

    applied styiling to the error page

--- src/pages/errorPage/ErrorPage.jsx
+++ src/pages/errorPage/ErrorPage.jsx
@@ -1,6 +1,16 @@
 import './errorPage.css';
+import { FaExclamationTriangle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
-  return <main>ErrorPage</main>;
+  return (
+    <main id="error-page">
+      <FaExclamationTriangle id="error-icon" />
+      <h2>404 Page Not Found!</h2>
+      <p>
+        Go back <Link to="/">Home</Link>.
+      </p>
+    </main>
+  );
 };
 export default ErrorPage;
